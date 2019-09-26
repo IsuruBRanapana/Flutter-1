02e33b51d20711b686e96bb780c390589b0ee626
Charunirathnayake <charunirathnayake1995@gmail.com>
2019-09-26 15:17:20 +0530

    Learning objectives

--- Flutter/sample_project/lib/main.dart
+++ Flutter/sample_project/lib/main.dart
@@ -1,8 +1,15 @@
 import 'package:flutter/material.dart';
 void main(){
   runApp(
-    Text("Hello Flutter",textDirection: TextDirection.ltr
+    MaterialApp(
+      title:"My Flutter App",home: new Material(color: Colors.greenAccent,child:Center(
+      child:Text(
+      "Hello Flutter",textDirection: TextDirection.ltr,style: TextStyle(color: Colors.black45,fontSize: 40.0),
     )
-  );
+  ) 
+  ,)
+  ,)
+    
+);
 
 }
