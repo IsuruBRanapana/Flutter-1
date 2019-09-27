28d87a96f9226e14a6f0bb04656ce62633125887
Charunirathnayake <charunirathnayake1995@gmail.com>
2019-09-27 09:28:15 +0530

    learning objectives of Flutter

--- Flutter/sample_project/lib/main.dart
+++ Flutter/sample_project/lib/main.dart
@@ -2,13 +2,19 @@ import 'package:flutter/material.dart';
 void main(){
   runApp(
     MaterialApp(
-      title:"My Flutter App",home: new Material(color: Colors.greenAccent,child:Center(
+      title:"My Flutter App",home:Scaffold(appBar: AppBar(title: Text("My first app"),)
+      ,body:new Material(color: Colors.blueAccent,child:Center(
       child:Text(
       "Hello Flutter",textDirection: TextDirection.ltr,style: TextStyle(color: Colors.black45,fontSize: 40.0),
     )
   ) 
   ,)
-  ,)
+
+      )
+      
+      
+      
+         ,)
     
 );
 
