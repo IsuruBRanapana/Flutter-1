9b4c96f9dcd026e6d7ff328df2515b0584a9f76a
Charunirathnayake <charunirathnayake1995@gmail.com>
2019-09-29 15:34:06 +0530

    Learning objectives of flutter

--- Flutter/sample_project02/lib/app_screen/home.dart
+++ Flutter/sample_project02/lib/app_screen/home.dart
@@ -9,13 +9,56 @@ class Home extends StatelessWidget{
       alignment: Alignment.center,
       margin: EdgeInsets.all(15.0),
       padding: EdgeInsets.all(10.0),
-      child: Text("Hello",textDirection: TextDirection.ltr,
+      child:Column(children: <Widget>[
+        Row(children: <Widget>[
+        Expanded(child:
+         Text("Hello",textDirection: TextDirection.ltr,
       style: TextStyle(color: Colors.black87,
       decoration: TextDecoration.none,
-      fontSize: 65.0,
-       ),),
+      fontSize: 45.0,
+       )
+       ,),),
+
+    Expanded(child: 
+        Text("Hello",textDirection: TextDirection.ltr,
+      style: TextStyle(color: Colors.black87,
+      decoration: TextDecoration.none,
+      fontSize: 45.0,
+       )
+       ,),)
    
-    )
+   
+
+      ],),
+      
+          Row(children: <Widget>[
+        Expanded(child:
+         Text("Hey",textDirection: TextDirection.ltr,
+      style: TextStyle(color: Colors.black87,
+      decoration: TextDecoration.none,
+      fontSize: 45.0,
+       )
+       ,),),
+
+    Expanded(child: 
+        Text("Hey",textDirection: TextDirection.ltr,
+      style: TextStyle(color: Colors.black87,
+      decoration: TextDecoration.none,
+      fontSize: 45.0,
+       )
+       ,),)
+   
+   
+
+      ],)
+      
+     
+  
+  
+
+      ],)
+      
+         )
      ,);
   }
 }
