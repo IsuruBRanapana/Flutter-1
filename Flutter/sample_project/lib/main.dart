1e91d18ef04db13a49081feae80782e78295158f
Isuru B. Ranapana <31366873+IsuruBRanapana@users.noreply.github.com>
2019-10-03 15:32:25 +0530

    Update main.dart

--- Flutter/sample_project/lib/main.dart
+++ Flutter/sample_project/lib/main.dart
@@ -2,20 +2,28 @@ import 'package:flutter/material.dart';
 void main(){
   runApp(
     MaterialApp(
-      title:"My Flutter App",home:Scaffold(appBar: AppBar(title: Text("My first app"),)
-      ,body:new Material(color: Colors.blueAccent,child:Center(
-      child:Text(
-      "Hello Flutter",textDirection: TextDirection.ltr,style: TextStyle(color: Colors.black45,fontSize: 40.0),
+      title:"My Flutter App",
+      home:Scaffold(
+        appBar: AppBar(
+          title: Text(
+            "My first app",
+            TextStyle(
+              color:Colors.white,
+              fontSize:20
+            )
+          ),
+        ),
+        body:new Material(
+          color: Colors.blueAccent,
+          child:Center(
+            child:Text(
+              "Hello Flutter",
+              textDirection: TextDirection.ltr,style:
+              TextStyle(color: Colors.black45,fontSize: 40.0),
+            )
+          ),
+        )
+      ),
     )
-  ) 
-  ,)
-
-      )
-      
-      
-      
-         ,)
-    
-);
-
+  );
 }
